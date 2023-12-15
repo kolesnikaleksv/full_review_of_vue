cfc39219e77166dc4b833a6df0a3e56096274664
kolesnikaleksv <kolesnikaleksv@gmail.com>
2023-12-15 08:41:21 +0200

    Add animation for moving and sorting elements of list

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,13 +1,15 @@
 <template>
    <div>
-    <h3>List of posts</h3>
-    <PostItem 
+    <h2>List of posts</h2>
+    <transition-group name="list-post">
+      <PostItem 
       v-if="posts.length > 0"
       v-for="post in posts" 
       :post="post"
       :key="post.id"
       @remove="$emit('remove', post)"/>
-    <h2 v-else>There are not post here. Create your one</h2>
+      <h2 v-else>There are not post here. Create your one</h2> 
+    </transition-group>
    </div>
 </template>
 
@@ -26,3 +28,35 @@ export default {
   }
 }
 </script>
+
+<style>
+/* .list-post-enter-active,
+.list-post-leave-active {
+  transition: all 0.5s ease;
+}
+.list-post-enter-from,
+.list-post-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+} */
+.list-post-move,
+.list-post-enter-active,
+.list-post-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-post-enter-from,
+.list-post-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+
+.list-post-leave-active {
+  position: absolute;
+}
+/* sort animation */
+.list-post-move {
+  transition: transform 0.8s ease;
+}
+</style>
